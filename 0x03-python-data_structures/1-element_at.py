8d1737c890bf57c683a451264c1e028c1d90ecbc
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-13 12:14:04 +0200

    t2 update

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     """get element at index"""
-    l = len(my_list)
-    if idx >= l or idx < 0:
+    list_len = len(my_list)
+    if idx >= list_len or idx < 0:
         return None
 
     return my_list[idx]
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     """replace list item at index"""
-    l = len(my_list)
-    if idx >= l or idx < 0:
+    list_len = len(my_list)
+    if idx >= list_len or idx < 0:
         return my_list
     my_list[idx] = element
     return my_list
