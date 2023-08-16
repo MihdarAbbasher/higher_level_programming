52b23348b04af881e620e2e1be4ca4312830e17f
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-17 00:11:53 +0200

    t100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+   if not my_list:
+        return 0
+    total_sum = 0
+    no_of_items = 0
+
+    for pair in my_list:
+        total_sum += pair[0] * pair[1]
+        no_of_items += pair[1]
+
+    return (total_sum / no_of_items)
