33dffb8d19c9405e3bbeea18a14e7de1647f8e50
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-16 08:04:09 -0700

    t4

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    li = []
+    for x in set_1:
+        if x not in set_2:
+            li.append(x)
+    for x in set_2:
+        if x not in set_1:
+            li.append(x)
+    return set(li)
