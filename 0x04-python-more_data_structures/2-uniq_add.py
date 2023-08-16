75d327c4b05038604a77ba174f6ecfcf318a0323
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-16 07:54:03 -0700

    t1 update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    sum = 0
     li = []
     for x in my_list:
         if x not in li:
             li.append(x)
-    return li
+            sum += x
+    return sum
