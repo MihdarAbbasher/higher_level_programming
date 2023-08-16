4326db37ac59666717555722565ea70ac9466313
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-16 08:15:13 -0700

    t5 updatee

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -2,7 +2,7 @@
 def number_keys(a_dictionary):
     res = 0
     li = []
-    for k, v if a_dictionary:
+    for k in a_dictionary:
         if k not in li:
             li.append(k)
             res += 1
