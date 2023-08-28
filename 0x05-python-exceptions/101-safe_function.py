c1718646a4fe56e0a210fb0d5fb7be4cedc2c859
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-28 07:22:05 -0700

    t101

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from sys import stderr
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+        return res
+    except Exception as e:
+        stderr.write("Exception: " + str(e) + "\n")
+        return
