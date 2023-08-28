8112585717a20822eed07943d281b695345d4e28
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-28 02:49:07 -0700

    t0 func style 3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    """ safe_print_list
+    """ safe_print_list.
+
     Args:
     list
 
@@ -17,3 +18,4 @@ def safe_print_list(my_list=[], x=0):
     except:
         print()
     return(i)
+
