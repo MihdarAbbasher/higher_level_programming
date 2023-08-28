f67d10be6ceedda9b9f694a097447e0d00796cc2
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-28 07:27:16 -0700

    t0 style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
-    """ safe_print_list.
-
-    Args:
-    list
-
-    Return:
-    count
-    """
     try:
         i = 0
         while(i < x):
