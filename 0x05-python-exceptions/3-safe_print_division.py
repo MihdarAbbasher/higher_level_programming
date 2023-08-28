accdd5fb0ec187460105682758d323233172fad0
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-28 05:30:12 -0700

    t3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    """ safe_print_ division.
+
+    Args:
+    a
+    b
+
+    Return:
+    result
+    """
+
+    try:
+        res = a / b
+    except(ZeroDivisionError):
+        res = None
+    finally:
+        print("Inside result: {}".format(res))
+    return(res)
