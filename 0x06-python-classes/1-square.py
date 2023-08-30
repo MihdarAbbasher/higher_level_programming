a16f65438df53b6d81be5de5f46dc2ec6cb8441a
MihdarAbbasher <mihdar.ph@gmail.com>
2023-08-30 06:50:26 -0700

    t1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module Squire that contain empty class.
+
+this is a square class with attr.
+"""
+
+
+class Square:
+    """empty square proto type."""
+
+    def __init__(self, size):
+        """initialize instance with size
+            Args:
+                size: size of square
+        """
+
+        self.__size = size
